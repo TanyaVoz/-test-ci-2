a6fd7f3d8d6ae8d53b4eff758ba49c42b19a3c68
Tanya <voznyuk1998@mail.ru>
2022-09-05 13:57:50 +0300

    fifish

---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,17 @@
+import healthIndicator from '../app';
+
+const heroes = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+const gradation = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+const result = healthIndicator(heroes);
+
+test('toEqual', () => {
+  expect(result).toEqual(gradation);
+});
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+// TODO: write your code here
+export default function healthIndicator(arr) {
+    arr.sort((a, b) => b.health - a.health);
+    return arr;
+}
